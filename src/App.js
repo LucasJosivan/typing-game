501e750152d7e3763d1a2734ea1fa4c0488fd276
LucasJosivan <lucasjosivam@hotmail.com>
2021-09-18 19:56:01 -0300

    creating backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import wordList from './resourses/words.json';
+
+const MAX_TYPED_KEYS = 30;
+
+const getWord = () => {
+    const index = Math.floor(Math.random() * wordList.length);
+    const word = wordList[index];
+    return word.toLowerCase();
+}
+
+const isValidKey = (key, word) =>  {
+    if (!word) return false;
+    const result = word.split('').includes(key);
+    return result;
+}
+
+const Word = ({word, validKeys}) => {
+    if (!word) return null;
+    const joinedKeys = validKeys.join('');
+    const matched = word.slice(0, joinedKeys.length);
+    const remainder = word.slice(joinedKeys.length);
+    return (<>
+        <span className="matched">{matched}</span>
+        <span className="remainder">{remainder}</span>
+        </>)
+}
 
 const App = () => {
-    return (<div className="container">
+    const [typedKeys, setTypedKeys] = useState([])
+    const [validKeys, setvalidKeys] = useState([])
+    const [word, setWord] = useState('');
+    useEffect(() => {
+        setWord(getWord());
+    }, []);
+    const handleKeyDown = (event) => {
+        event.preventDefault();
+        const { key } = event;
+        setTypedKeys((prevTypedKeys)=>{
+            return [...prevTypedKeys, key].slice(MAX_TYPED_KEYS * -1);
+        });
+        if (isValidKey(key, word)) {
+            setvalidKeys((prevTypedKeys) => {
+                const isValidLength = prevTypedKeys.length <= word.length;
+                const isNextChar = isValidLength && word[prevTypedKeys.length] === key;
+                return (isNextChar) ? [...prevTypedKeys, key] : prevTypedKeys;
+
+            })
+        }
+    }
+    return (<div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
         <div className="valid-keys">
-            <span className="matched">Luc</span>
-            <span className="remainder">as</span>
+            <Word word={word} validKeys={validKeys} />
         </div>
-        <div className="typed-keys">daskgjnlhsujscggd</div>
+        <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
         <div className="completed-words">
             <ol>
                 <li>cidade</li>
